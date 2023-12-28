a613b0a274ca6f7de341fe8ee72d21b997ceace5
Digvijay <digbjssn@gmail.com>
2023-12-29 02:20:46 +0530

    test route

---
+++ travel-backend/controller/user-controller.js
@@ -0,0 +1,8 @@
+// user-controller.js
+const test = (req, res) => {
+    res.json({
+        message: "I am in controller file",
+    });
+};
+
+module.exports = test;
--- travel-backend/index.js
+++ travel-backend/index.js
@@ -2,15 +2,21 @@ const express=require('express');
 const mongoose=require('mongoose')
 const dotenv=require('dotenv');
       dotenv.config();
+const adminRouter=require('./routes/user.route')
 const app=express();
 
 
+//Connecting Mongodb
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log('connected to MongoDB')
 }).catch((err)=>{
     console.log(err)
 })
 
+
+//Creating the server
 app.listen(8000,()=>{
     console.log('Server is Live at 8000 port');
 })
+
+app.use('/test',adminRouter);
